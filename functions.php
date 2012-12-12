3b77394d165a29977d8066d7b82d2aa6e5cbf4d0
chrisramey <cjramey@gmail.com>
2012-12-12 11:21:55 -0600

    Setup theme structure with template files

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+// Register a single navigation menu, to be
+// constructed in the admin panel, and
+// displayed in nav.php
+register_nav_menus(array(
+	'primary' => 'Primary Navigation'
+));
