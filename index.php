07ae1fafa4debb8a143283d29a5d9c0ba6e9bc85
chrisramey <cjramey@gmail.com>
2012-12-13 11:29:41 -0600

    Added sidebar and more content to each template

--- index.php
+++ index.php
@@ -12,31 +12,50 @@
 				<?php get_template_part('nav') // includes nav.php ?>
 			</nav>
 			<div id="main">
-				<?php 
-				/**
-				 * Check to see if the current request...
-				 * is_category()
-				 * is_front_page()
-				 * is_home()
-				 * is_404()
-				 * is_archive()
-				 * is_page()
-				 * is_single() --> e.g. a single post
-				 * is_tag()
-				 * is_search() --> a page display search results
-				 */
-				if (is_front_page()) {
+				<div class="row">
+					<!-- MAIN COLUMN -->
+					<div class="span9">
+						<?php 
+						/**
+						 * Check to see if the current request...
+						 * is_category()
+						 * is_front_page()
+						 * is_home()
+						 * is_404()
+						 * is_archive()
+						 * is_page()
+						 * is_single() --> e.g. a single post
+						 * is_tag()
+						 * is_search() --> a page display search results
+						 */
+						if (is_front_page()) {
+							
+							// Include file named content-home.php
+							get_template_part('content','home');
+							
+						} elseif (is_page() || is_single()) {
+							
+							// Include file named content-single.php
+							get_template_part('content','single');
+							
+						} elseif (is_search()) {
+							
+							// Include file named content-search.php
+							get_template_part('content','search');
+						} else { // Display posts!
+							
+							// Include file named content-blog.php
+							get_template_part('content','blog');
+						}
+						?>
+					</div>
 					
-					// Include file named content-home.php
-					get_template_part('content','home');
-					
-				} elseif (is_page() || is_single()) {
-					
-					// Include file named content-single.php
-					get_template_part('content','single');
-					
-				}
-				?>
+					<!-- RIGHT SIDEBAR -->
+					<div class="span3">
+						<?php get_sidebar() ?>
+					</div>
+				</div>
+				
 			</div>
 			<footer>
 				<?php get_footer() ?>
---
+++ sidebar.php
@@ -0,0 +1,7 @@
+<!-- SEARCH FORM -->
+<form class="form" action="<?php bloginfo('url')?>">
+	<div class="input-append">
+		<input class="span2" type="text" name="s" placeholder="search" />
+		<button class="btn" type="submit"><i class="icon-search"></i></button>
+	</div>
+</form>
